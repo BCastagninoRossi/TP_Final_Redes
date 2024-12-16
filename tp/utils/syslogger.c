7829429d39bb95863de38777265979a1d194dad2
AgusManzano <amanzano@udesa.edu.ar>
2024-12-16 18:48:13 -0300

    final

--- tp/utils/syslogger.c
+++ tp/utils/syslogger.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <syslog.h>
 #include <string.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-#define SYSLOG_SERVER "127.0.0.1"  // Use loopback IP address
 #define SYSLOG_PORT 514
+#define SYSLOG_SERVER "127.0.0.1"
 #define HOSTNAME "localhost"
 #define PRIORITY 163
 
@@ -16,7 +14,7 @@ int syslog_socket;
 struct sockaddr_in syslog_addr;
 
 void init_syslog_connection() {
-    // Create a UDP socket
+    // Create a socket for sending syslog messages
     syslog_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (syslog_socket < 0) {
         perror("Error creating socket");
@@ -37,14 +35,14 @@ void init_syslog_connection() {
 void log_message_syslog(const char *timestamp, const char *conversation_id, const char *role, const char *message, const char *sentiment, float sentiment_value) {
     // Format the message string
     char log_msg[10000];
-    snprintf(log_msg, sizeof(log_msg), "<%d> %s %s %s %s %s (sentiment: %s sentiment_value: %.1f)",
-             PRIORITY, timestamp, HOSTNAME, conversation_id, role, message, sentiment, sentiment_value);
+    snprintf(log_msg, sizeof(log_msg), "<163> %s %s %s %s %s (sentiment: %s sentiment_value: %.1f)",
+             timestamp, HOSTNAME, conversation_id, role, message, sentiment, sentiment_value);
 
     // Send the message to the syslog server
     int bytes_sent = sendto(syslog_socket, log_msg, strlen(log_msg), 0, (struct sockaddr *)&syslog_addr, sizeof(syslog_addr));
     if (bytes_sent < 0) {
         perror("Error sending syslog message");
     } else {
-        printf("Syslog message sent: %s\n", log_msg);
+        // printf("Syslog message sent: %s\n", log_msg);
     }
 }
